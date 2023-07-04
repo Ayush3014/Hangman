d718722c7a20dfab9141cdcd130595fc357141a6
Ayush <ayushpanchal005@gmail.com>
2023-07-04 15:27:57 +0530

    Functions to update the hidden letters and checks

--- lib/game.rb
+++ lib/game.rb
@@ -24,40 +24,79 @@ class Game
     hide_word(lines[random_index].chomp)    # get the line from the index and remove the newline character using chomp
   end
 
-  def hide_word(word)
-    correct_word = word.split('')               # split the word into an array of letters
-    word = word.downcase.gsub(/[a-z]/, '_')     # replace the letters by _
-    puts word
+  def hide_word(hidden_word)
+    correct_word = hidden_word.split('')               # split the word into an array of letters
+    hidden_word = hidden_word.downcase.gsub(/[a-z]/, '_')     # replace the letters by _
+    puts hidden_word
     puts correct_word.join()                    # print the original word as a string
-    play(correct_word)
+    play(correct_word, hidden_word)
   end
 
-  def user_input
+  def user_input(used_letters)
     input = ""
+
     loop do
       puts "Enter a letter: "
       input = gets.chomp.downcase
+      if(used_letters.join().include?(input))
+        puts "\e[31m Please enter a different letter!! \e[0m"
+        next
+      end
       break if input.match?(/\A[a-z]\z/)
       puts "\e[31m Invalid input! Please enter a single letter!!\e[0m"
     end
+
     input
   end
 
-  def play(correct_word)
+  def play(correct_word, hidden_word)
     count = 0
     used_letters = []
+
     while count < 10
-      input = user_input
+      puts "\n"
+      input = user_input(used_letters)
       used_letters << input
-      count += 1
+
+      if(correct_word.include?(input))
+        puts "\e[44mCorrect Guess!\e[0m"
+        replace_letter(input, correct_word, hidden_word)
+
+        if hidden_word.include?('_')
+          next
+        else
+          won(count)
+          break
+        end
+      else
+        count += 1
+      end
+
       puts "Letters used: #{used_letters.join(" ")}"
       puts "\e[31mRemaining tries: #{10 - count}\e[0m" unless count == 9
     end
+    if count == 10
+          lose(correct_word)
+        end
+
+  end
+
+  def replace_letter(input, correct_word, hidden_word)
+    correct_word.each_with_index do |letter, index|
+      hidden_word[index] = input if letter == input
+    end
+    p hidden_word
+  end
+
+  def won(count)
+    puts "You guessed the word correctly! You win!! "
+  end
+
+  def lose(correct_word)
     puts "\e[31m You lost!! \e[0m"
-    puts "The correct word is: #{correct_word.join()}"
+    puts "The correct word is: \e[31m#{correct_word.join()}\e[0m"
   end
 end
-
 
 random_word = Game.new
 random_word.display_random_word('google-10000-english-no-swears.txt')
